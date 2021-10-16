889f1e12d72e0047f2c03179cf6a8be7fa0b8f07
Kevin Salina <kevin.salina25@gmail.com>
2021-10-15 21:33:52 -0400

    Converted cloud functions to Express Routes.

---
+++ dbschema.js
@@ -0,0 +1,14 @@
+let db = {
+  rounds: [
+    {
+      userHandle: 'userName',
+      course: 'Course Name',
+      numHoles: 9,
+      par: 36,
+      score: 41,
+      createdAt: '2021 - 10 - 16T01: 26: 34.353Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,50 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
+const express = require('express')
+const app = express()
 
 // Firebase App Initi
 admin.initializeApp()
 const db = admin.firestore()
 
 // Get All Golf Rounds
-exports.getRounds = functions.https.onRequest(async (req, res) => {
+app.get('/rounds', async (req, res) => {
   try {
-    const snapshot = await db.collection('rounds').get()
+    const snapshot = await db.collection('rounds').orderBy('createdAt', 'desc').get()
     let allRounds = []
     snapshot.forEach(doc => {
-      allRounds.push(doc.data())
+      allRounds.push({
+        roundId: doc.id,
+        ...doc.data()
+      })
     })
+
     return res.json(allRounds)
+
   } catch (err) {
     console.error(err)
+    return res.status(500).json({ error: 'Cannot find golf rounds.' })
   }
 })
 
-exports.createRound = functions.https.onRequest(async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed' })
-  }
+// Create new Round
+app.post('/round', async (req, res) => {
   try {
-    const { course, numHoles, par, score, userHandle } = req.body
     const newRound = {
-      course,
-      numHoles,
-      par,
-      score,
-      userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date())
+      ...req.body,
+      createdAt: new Date().toISOString()
     }
 
     const addedRound = await db.collection('rounds').add(newRound)
-    res.json({ message: `Round ${addedRound.id} created successfully!` })
+
+    return res.json({ message: `Round ${addedRound.id} created successfully!` })
+
   } catch (err) {
-    res.status(500).json({ error: 'Something went wrong' })
     console.error(err)
+    return res.status(500).json({ error: 'Something went wrong' })
   }
 })
+
+
+// Export all routes with /api
+exports.api = functions.https.onRequest(app);
