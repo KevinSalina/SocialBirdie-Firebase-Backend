da3f610cd633e4bb4586392bc734c97500219664
Kevin Salina <kevin.salina25@gmail.com>
2021-10-19 00:11:40 -0400

    Created middleware function that checks for authorization header in request. If so, verifies ID Token. If ID Token is verified, user can proceed to protected routes.

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,37 @@ admin.initializeApp({
 })
 const db = admin.firestore()
 
+// Middleware - Is user authorized?
+const FBAuth = async (req, res, next) => {
+  let idToken;
+
+  try {
+    // Check if req has header of auth and it starts with firebase convention of 'Bearer'
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+      idToken = req.headers.authorization.split('Bearer ')[1]
+    } else {
+      console.error('No token found')
+      return res.status(403).json({ error: 'Unauthorized' })
+    }
+
+    // If we have token, and it is verified, add to request body so we can use in response
+    const verifyToken = await admin.auth().verifyIdToken(idToken)
+    console.group(verifyToken)
+    req.body.user = verifyToken
+
+    // Get assocaited user, and add to request body
+    const user = await db.collection('users').where('userId', '==', verifyToken.uid).limit(1).get()
+    req.body.username = user.docs[0].data().username
+
+    return next()
+
+  } catch (err) {
+    console.error('Error while verifying token ', err)
+    return res.status(403).json(err)
+  }
+}
+
+
 // Get All Golf Rounds
 app.get('/rounds', async (req, res) => {
   try {
@@ -33,10 +64,17 @@ app.get('/rounds', async (req, res) => {
 })
 
 // Create new Round
-app.post('/round', async (req, res) => {
+app.post('/round', FBAuth, async (req, res) => {
+
+  const { username, course, numHoles, par, score } = req.body
+
   try {
     const newRound = {
-      ...req.body,
+      username,
+      course,
+      numHoles,
+      par,
+      score,
       createdAt: new Date().toISOString()
     }
 
@@ -50,13 +88,15 @@ app.post('/round', async (req, res) => {
   }
 })
 
-// Helpher Function for empty string
+// Helper Function for empty string
 const isEmpty = string => string.trim() === ''
 // Helper function for email check
 const isValidEmail = (email) => {
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegEx.test(email)
 }
+
+
 // Sign Up Route
 app.post('/signup', async (req, res) => {
 
@@ -131,8 +171,8 @@ app.post('/login', async (req, res) => {
   } catch (err) {
     if (err.response) {
       console.log('1st', err.response.data)
-      if (err.response.data.error.message === "EMAIL_NOT_FOUND") errors.email = 'Incorrect Email. Please try again'
-      if (err.response.data.error.message === "INVALID_PASSWORD") errors.password = 'Incorrect Password. PLease try again'
+      if (err.response.data.error.message === "EMAIL_NOT_FOUND") errors.email = 'Incorrect Credentials. Please try again'
+      if (err.response.data.error.message === "INVALID_PASSWORD") errors.password = 'Incorrect Credentials. PLease try again'
       return res.status(400).json(errors)
     } else if (err.request) {
       console.log('2nd', err.request)
